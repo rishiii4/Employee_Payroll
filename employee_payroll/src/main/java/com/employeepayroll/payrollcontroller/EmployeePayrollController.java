67f4c1a83255747a247323a9fb66007a7a0c4e0f
Pranav Shrivastav <rishishrivastav2211@gmail.com>
2025-02-25 15:12:45 +0530

    [Pranav] Add. Testing Connectivity

--- employee_payroll/src/main/java/com/employeepayroll/payrollcontroller/EmployeePayrollController.java
+++ employee_payroll/src/main/java/com/employeepayroll/payrollcontroller/EmployeePayrollController.java
@@ -1,17 +1,20 @@
 package com.employeepayroll.payrollcontroller;
 
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/employeepayrollservice")
 public class EmployeePayrollController {
 
-    // Testing the server using CURL Commands
+
+    // Testing the Connectivity
     @GetMapping
-    public String greet(){
-        return "Hello, This is Employee Payroll!";
+    public ResponseEntity<String> test(){
+        return ResponseEntity.ok("Connection Successful!");
     }
+
 
 }
 
