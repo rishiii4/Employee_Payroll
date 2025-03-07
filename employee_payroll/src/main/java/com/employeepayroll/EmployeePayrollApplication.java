452d92f7e5e88f6c40bd27a18b5e54315405dfbe
Pranav Shrivastav <rishishrivastav2211@gmail.com>
2025-03-07 18:52:41 +0530

    [Pranav] Add. DataBase Setting as Environment Variable

--- employee_payroll/src/main/java/com/employeepayroll/EmployeePayrollApplication.java
+++ employee_payroll/src/main/java/com/employeepayroll/EmployeePayrollApplication.java
@@ -1,13 +1,18 @@
 package com.employeepayroll;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollApplication.class, args);
+		ApplicationContext context = SpringApplication.run(EmployeePayrollApplication.class,args);
+		log.info("Employee Payroll App Started in {} Environment",context.getEnvironment().getProperty("environment"));
+		log.info("Employee Payroll DB User is {}",context.getEnvironment().getProperty("spring.datasource.username"));
 	}
 
 }
